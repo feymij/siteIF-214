7baa322cec24cfce47fdfbdbeb4a15856b33de0e
Aylin <btwnouser0@gmail.com>
2025-11-30 20:50:04 +0200

    end

--- docs/js/script.js
+++ docs/js/script.js
@@ -3,5 +3,28 @@
         btn1.addEventListener('click', () => {
             var name = input_name.value;
             console.log(name);
-            alert(`Hello ${name}`);
+            alert(`Thank you ${name}, for visiting this site.
+All information taken from Wikipedia and slightly supplemented 
+Link:
+https://en.wikipedia.org/wiki/Friedrich_Nietzsche`);
         });
+        
+        const images = document.querySelectorAll('.gallery img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const closeBtn = document.querySelector('.close');
+
+images.forEach(img => {
+    img.addEventListener('click', () => {
+        lightbox.style.display = 'block';
+        lightboxImg.src = img.src;
+    });
+});
+
+closeBtn.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+});
+
+lightbox.addEventListener('click', (e) => {
+    if (e.target === lightbox) lightbox.style.display = 'none';
+});
